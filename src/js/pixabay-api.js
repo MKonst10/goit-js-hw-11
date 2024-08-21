06989c3722b89e72bc3a4b97451181f96cc40f59
MKonst <mkonst10@gmail.com>
2024-08-21 12:14:35 +0300

    replace functions

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,4 @@
-import { createGallery } from './render-functions';
-import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-const loader = document.querySelector('.loader');
 
 export const fetchPhotos = searchedValue => {
   const fetchParams = new URLSearchParams({
@@ -12,32 +9,5 @@ export const fetchPhotos = searchedValue => {
     key: '45532957-9c3fb9f5acbf0ac0bdba7e21a',
   });
 
-  loader.classList.remove('is-hidden');
-  return fetch(`https://pixabay.com/api/?${fetchParams}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data.hits);
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          close: 'true',
-          position: 'topRight',
-          color: '#ef4040',
-          messageColor: 'white',
-        });
-      }
-      createGallery(data);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => {
-      loader.classList.add('is-hidden');
-    });
+  return fetch(`https://pixabay.com/api/?${fetchParams}`);
 };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { fetchPhotos } from './js/pixabay-api';
+import { createGallery } from './js/render-functions';
 
 const gallery = document.querySelector('.gallery');
 const searchForm = document.querySelector('.form');
 const requestInput = document.querySelector('.search-input');
-const searchBtn = document.querySelector('.search-btn');
+const loader = document.querySelector('.loader');
+
 let searchedValue;
 
-const onBtnClick = event => {
+const onFormSubmit = event => {
   event.preventDefault();
   if (requestInput.value === '') {
     iziToast.error({
@@ -20,12 +22,43 @@ const onBtnClick = event => {
     });
   } else {
     searchedValue = requestInput.value;
-    fetchPhotos(searchedValue);
+    loader.classList.remove('is-hidden');
+    fetchPhotos(searchedValue)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data.hits);
+        if (data.hits.length === 0) {
+          iziToast.error({
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            close: 'true',
+            position: 'topRight',
+            color: '#ef4040',
+            messageColor: 'white',
+          });
+        }
+        createGallery(data);
+      })
+      .catch(error => {
+        iziToast.error({
+          message: `An error ${error} occurred`,
+          close: 'true',
+          position: 'topRight',
+          color: '#ef4040',
+          messageColor: 'white',
+        });
+      })
+      .finally(() => {
+        loader.classList.add('is-hidden');
+      });
   }
   searchForm.reset();
   gallery.innerHTML = '';
 };
 
-searchBtn.addEventListener('click', onBtnClick);
-
-gallery.addEventListener('click', event => event.preventDefault());
+searchForm.addEventListener('submit', onFormSubmit);
